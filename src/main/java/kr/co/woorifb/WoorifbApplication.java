726e55b3ca22e3e1085be83806067795b4ffa9bb
dongqda <dglee012@gmail.com>
2020-03-23 20:48:05 +0900

    spring initialize

---
+++ src/main/java/kr/co/woorifb/WoorifbApplication.java
@@ -0,0 +1,13 @@
+package kr.co.woorifb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorifbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoorifbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/woorifb/WoorifbApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.woorifb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorifbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
